3b05369d7c8a20e0d248e7ee5076088651d572ed
@dankomorny <DanKomorny@users.noreply.github.com>
2024-05-04 22:14:29 +0200

    Update main.py
    Basic project stub

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-def main():
-    """ Main entry point of the app """
-    print("hello world")
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('M30W').build()
     
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
